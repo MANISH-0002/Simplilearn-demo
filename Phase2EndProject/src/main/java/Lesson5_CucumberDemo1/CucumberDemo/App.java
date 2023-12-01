565dac63077182a29150a2455fa265e88266d6e8
MANISH-0002 <65907722+MANISH-0002@users.noreply.github.com>
2023-12-01 18:52:10 +0530

    Phase2EndProject

---
+++ Phase2EndProject/src/main/java/Lesson5_CucumberDemo1/CucumberDemo/App.java
@@ -0,0 +1,13 @@
+package Lesson5_CucumberDemo1.CucumberDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Phase2EndProject/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"d5e512cd-b497-4668-bedd-ed899596f762","feature":"Test the starHealth page on Chrome Browser","scenario":"Validate the Star Health Buy Now flow","start":1701434821768,"group":1,"content":"","tags":"","end":1701434858136,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
